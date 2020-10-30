7d6f0da0ce78e7f74e507a25b99b671d43310914
Joseph Zhang <josephzhang@cmu.edu>
2020-10-30 00:36:57 -0400

    restructure data

---
+++ js-versions/main3.js
@@ -0,0 +1,205 @@
+//Books API — https://developer.nytimes.com/docs/books-product/1/routes/lists.json/get
+
+let articleObjs = [];
+let articleList = [];
+let counter = 0;
+let apikey = "dPxVkGTEZ2h4KpgLTnwr6TziqqeoQspR";
+
+let table;
+let tables = [];
+let years = [
+  2006,
+  2007,
+  2008,
+  2009,
+  2011,
+  2012,
+  2013,
+  2014,
+  2015,
+  2016,
+  2017,
+  2018,
+  2019,
+];
+let trends = [];
+let csvs = [
+  "2006.csv",
+  // "2007.csv",
+  // "2008.csv",
+  // "2009.csv",
+  // "2011.csv",
+  // "2012.csv",
+  // "2013.csv",
+  // "2014.csv",
+  // "2015.csv",
+  // "2016.csv",
+  // "2017.csv",
+  // "2018.csv",
+  // "2019.csv",
+];
+
+function preload() {
+  for (let i = 0; i < csvs.length; i++) {
+    table = loadTable("trends/" + csvs[i], "csv", "header");
+    tables.push(table);
+  }
+}
+
+function setup() {
+  csvToDict();
+  createCanvas(800, 4000);
+  for (let i = 0; i < trends.length; i++) {
+    trends[i].render();
+  }
+  getBooks();
+}
+
+function csvToDict() {
+  for (let h = 0; h < tables.length; h++) {
+    table = tables[h];
+    let cols = table.findRow().table.columns;
+    let dict = new Map(); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+    let key = new Map();
+    for (let i = 0; i < cols.length; i++) {
+      let t = table.getColumn(cols[i]);
+      key.set(cols[i], "");
+      for (let j = 0; j < t.length; j++) {
+        dict.set(cols[i] + j, t[j]);
+      }
+    }
+    trends.push(new Trend(years[h], key, dict));
+  }
+}
+
+function getVals(d, s) {
+  let temp = [];
+  for (let [key, value] of d) {
+    if (key.includes(s)) temp.push(value);
+  }
+  return temp;
+}
+
+class Trend {
+  constructor(y, k, d) {
+    this.id = y;
+    this.keys = k;
+    this.vals = d;
+    this.dates = "";
+    // this.x = random(width);
+    // this.y = random(height);
+  }
+  render() {
+    // text(this.id, 20, 20);
+    let year = this.id;
+    $(".trendList").append("<h1>" + year + "</1>");
+    for (let key of this.keys.keys()) {
+      $(".trendList").append("<h2>" + key + "</h2>");
+
+      getVals(this.vals, key);
+      let temp = getVals(this.vals, key);
+
+      for (let i = 0; i < temp.length; i++) {
+        let keyWord = temp[i];
+        $(".trendList").append("<p>" + keyWord + "</p>");
+
+        // for every trend, do a keyword search for articles
+        this.articleSearch(keyWord, year);
+      }
+    }
+  }
+  articleSearch(movieTitle, year) {
+    //q will be each trend
+    let q = movieTitle.split(" ").join("%20");
+    let startDate = year + "0101";
+    let endDate = year + "1231";
+    let pages = 1;
+
+    for (let i = 1; i < pages + 1; i++) {
+      let searchedArticles =
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json?" +
+        "begin_date=" +
+        startDate +
+        "&end_date=" +
+        endDate +
+        "&page=" +
+        i +
+        "&q=" +
+        q +
+        "&api-key=" +
+        apikey;
+
+      // https://hackernoon.com/limiting-your-api-requests-the-right-way-9608b661a0ce
+      $.getJSON(searchedArticles, (data) => this.afterPull(data));
+    }
+  }
+  afterPull(data) {
+    articleList.push(...data.response.docs);
+    //figure out time stamp between two dates
+    let months = [];
+    for (let i in articleList) {
+      let str = articleList[i].pub_date,
+        delimiter = "-",
+        start = 2,
+        tokens2 = str.split(delimiter).slice(1, start),
+        result2 = tokens2.join(); // this
+
+      //convert to num
+      let integer = parseInt(result2, 10);
+      months.push(integer);
+    }
+    let lastestMonth = Math.max(...months);
+    let earliestMonth = Math.min(...months);
+
+    //duration of this trend
+    let totalDuration = lastestMonth - earliestMonth;
+    if (isFinite(totalDuration)) {
+      // console.log(this.id);
+      this.dates = earliestMonth;
+      console.log(trends);
+      //MAP TIME BACK TO THE RELATED GOOGLE TRENDs
+    }
+  }
+}
+
+//pull books based on set date
+const getBooks = () => {
+  let publishedDate = "2015-06-02";
+  let bookCategories = [
+    "combined-print-and-e-book-fiction",
+    "combined-print-and-e-book-nonfiction",
+  ];
+  let bookList = [];
+  // let bestSellers =
+  //   "https://api.nytimes.com/svc/books/v3/lists.json?list=combined-print-and-e-book-fiction" +
+  //   "&published-date=" +
+  //   publishedDate +
+  //   "&api-key=" +
+  //   apikey;
+
+  for (let i = 0; i < bookCategories.length; i++) {
+    let bestSellers =
+      "https://api.nytimes.com/svc/books/v3/lists.json?list=" +
+      bookCategories[i] +
+      "&published-date=" +
+      publishedDate +
+      "&api-key=" +
+      apikey;
+
+    let bookNum = 10;
+    $.getJSON(bestSellers, function (data) {
+      for (let k = 0; k < bookNum; k++) {
+        let bookTitle = data.results[k].book_details[0].title;
+        bookList.push(bookTitle);
+        $(".bookList").append(
+          "<p>" +
+            bookTitle +
+            "(" +
+            data.results[i].published_date +
+            ") " +
+            "</p>"
+        );
+      }
+    });
+  }
+};
