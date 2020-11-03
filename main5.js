4e3b28cf3d48104362df3cc87bb9ba8fdaa93168
Joseph Zhang <josephzhang@cmu.edu>
2020-11-02 19:25:55 -0500

    font size increase

--- main5.js
+++ main5.js
@@ -58,14 +58,14 @@ function preload() {
     tables.push(table);
   }
   // convert books into data
-  // $.getJSON("books/books.json", (data) => {
-  //   // books = data;
-  //   // downloadBookData();
-  //   for (let i = 0; i < 300; i++) {
-  //     getRandomInt(1, 3, 2);
-  //     createTiles("hello");
-  //   }
-  // });
+  $.getJSON("books/books.json", (data) => {
+    // books = data;
+    // downloadBookData();
+    // for (let i = 0; i < 300; i++) {
+    //   getRandomInt(1, 3, 2);
+    //   createTiles("hello");
+    // }
+  });
 
   $.getJSON("books/book-description.json", (data) => {
     // booksWithDescriptions = data;
@@ -74,6 +74,7 @@ function preload() {
       getRandomInt(1, 3, 2);
       createTiles("hello");
     }
+    console.log("hello");
   });
 }
 
@@ -262,50 +263,50 @@ class TrendYear {
 }
 
 //FILTER BOOK
-filterSelection("all");
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
-}
+// filterSelection("all");
+// function filterSelection(c) {
+//   var x, i;
+//   x = document.getElementsByClassName("filterDiv");
+//   if (c == "all") c = "";
+//   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+//   for (i = 0; i < x.length; i++) {
+//     RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+//   }
+// }
 
-// Show filtered elements
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+// // Show filtered elements
+// function AddClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     if (arr1.indexOf(arr2[i]) == -1) {
+//       element.className += " " + arr2[i];
+//     }
+//   }
+// }
 
-// Hide elements that are not selected
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+// // Hide elements that are not selected
+// function RemoveClass(element, name) {
+//   var i, arr1, arr2;
+//   arr1 = element.className.split(" ");
+//   arr2 = name.split(" ");
+//   for (i = 0; i < arr2.length; i++) {
+//     while (arr1.indexOf(arr2[i]) > -1) {
+//       arr1.splice(arr1.indexOf(arr2[i]), 1);
+//     }
+//   }
+//   element.className = arr1.join(" ");
+// }
 
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// // Add active class to the current control button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
